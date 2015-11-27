ec847fb005f4c6bd7dc431c9304057dbb0f7db31
nicho <nicho@nicho-ubuntu>
2015-11-26 22:59:17 -0600

    Reorganized some code, set some accelerators and added functionality to the open/save menu items

--- ExtremeEditor/src/ca/nicho/gui/FontPanel.java
+++ ExtremeEditor/src/ca/nicho/gui/FontPanel.java
@@ -1,43 +1,46 @@
 package ca.nicho.gui;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSeparator;
-import javax.swing.JTextField;
-import javax.swing.JToolBar;
-import javax.swing.SwingConstants;
-import javax.swing.border.BevelBorder;
 
 import ca.nicho.action.HandlerAction;
 
 public class FontPanel extends JPanel{
 	
 	private ToolPanel toolPanel;
-	private CPPanel copypastePanel;
+	private CPPanel copyPastePanel;
 	private FileManagePanel fileManagePanel;	
 	
 	public FontPanel(HandlerAction handler, ActionHistoryFrame actions){
 		this.setLayout(new FlowLayout(FlowLayout.LEFT));
 		fileManagePanel = new FileManagePanel(handler, actions);
 		toolPanel = new ToolPanel(handler, actions);
-		copypastePanel = new CPPanel(handler,actions);
+		copyPastePanel = new CPPanel(handler,actions);
 		this.add(fileManagePanel);
 		this.add(toolPanel);
-		this.add(copypastePanel);
+		this.add(copyPastePanel);
 		this.setBorder(BorderFactory.createLoweredSoftBevelBorder());
 		this.setBackground(new Color(166,177,178));
 		this.setPreferredSize(new Dimension(40,50));
 
 	}
 
+	public ToolPanel getToolPanel(){
+		return toolPanel;
+	}
+	
+	public CPPanel getCPPanel(){
+		return copyPastePanel;
+	}
+	
+	public FileManagePanel getFilePanel(){
+		return fileManagePanel;
+	}
+	
+	
+	
 }
